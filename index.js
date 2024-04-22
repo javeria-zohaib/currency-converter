8f48084be809182198d93f6fc080826123566d26
javeria-zohaib <161313643+javeria-zohaib@users.noreply.github.com>
2024-04-22 13:11:59 -0700

    currency converter using typescript, node.js and inquirer

---
+++ index.js
@@ -0,0 +1,35 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+let exchange_rate = {
+    "USD": 1,
+    "EUR": 0.9,
+    "JYP": 113,
+    "CAD": 1.3,
+    "AUD": 1.65,
+    "PKR": 278.2,
+    "INR": 83.36
+};
+let userAnswer = await inquirer.prompt([{
+        name: "from_currency",
+        type: "list",
+        message: chalk.blueBright("Select the currency to convert from:"),
+        choices: ["USD", "EUR", "JYP", "CAD", "AUD", "PKR", "INR"],
+    },
+    {
+        name: "to_currency",
+        type: "list",
+        message: chalk.blueBright("Select the currency to convert into:"),
+        choices: ["USD", "EUR", "JYP", "CAD", "AUD", "PKR", "INR"],
+    },
+    {
+        name: "amount",
+        type: "input",
+        message: "enter the amount to convert:"
+    }
+]);
+let from_amount = exchange_rate[userAnswer.from_currency];
+let to_amount = exchange_rate[userAnswer.to_currency];
+let amount = userAnswer.amount;
+let baseAmount = amount / from_amount;
+let convertedAmount = baseAmount * to_amount;
+console.log(`Converted amount= ${convertedAmount}`);
